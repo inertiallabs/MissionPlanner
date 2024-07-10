ad670192baad6a7a5ba36897fe6f900709d4d852
Valentin Bugrov <valentin.bugrov@inertiallabs.com>
2024-07-11 03:30:53 +0400

    GCSViews: Add the context menu item I'm Here

--- GCSViews/FlightData.Designer.cs
+++ GCSViews/FlightData.Designer.cs
@@ -191,13 +191,13 @@ namespace MissionPlanner.GCSViews
             this.BUT_loganalysis = new MissionPlanner.Controls.MyButton();
             this.tabExternalAHRS = new System.Windows.Forms.TabPage();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.BUT_inertiallabs_start = new MissionPlanner.Controls.MyButton();
-            this.BUT_inertiallabs_stop = new MissionPlanner.Controls.MyButton();
-            this.BUT_inertiallabs_gnss_enable = new MissionPlanner.Controls.MyButton();
-            this.BUT_inertiallabs_gnss_disable = new MissionPlanner.Controls.MyButton();
-            this.BUT_inertiallabs_vg3dclb_flight_start = new MissionPlanner.Controls.MyButton();
-            this.BUT_inertiallabs_vg3dclb_flight_stop = new MissionPlanner.Controls.MyButton();
-            this.BUT_inertiallabs_aiding_data = new MissionPlanner.Controls.MyButton();
+            this.BUT_externalAHRS_gnss_enable = new MissionPlanner.Controls.MyButton();
+            this.BUT_externalAHRS_gnss_disable = new MissionPlanner.Controls.MyButton();
+            this.BUT_externalAHRS_vg3dclb_flight_start = new MissionPlanner.Controls.MyButton();
+            this.BUT_externalAHRS_vg3dclb_flight_stop = new MissionPlanner.Controls.MyButton();
+            this.BUT_externalAHRS_start = new MissionPlanner.Controls.MyButton();
+            this.BUT_externalAHRS_stop = new MissionPlanner.Controls.MyButton();
+            this.BUT_externalAHRS_aiding_data = new MissionPlanner.Controls.MyButton();
             this.panel_persistent = new System.Windows.Forms.Panel();
             this.tableMap = new System.Windows.Forms.TableLayoutPanel();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
@@ -226,6 +226,7 @@ namespace MissionPlanner.GCSViews
             this.gimbalVideoMiniToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.gimbalVideoPopOutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
+            this.imHereToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.but_disablejoystick = new MissionPlanner.Controls.MyButton();
             this.Zoomlevel = new System.Windows.Forms.NumericUpDown();
             this.distanceBar1 = new MissionPlanner.Controls.DistanceBar();
@@ -414,8 +415,8 @@ namespace MissionPlanner.GCSViews
             this.hud1.disttowp = 0F;
             this.hud1.distunit = null;
             resources.ApplyResources(this.hud1, "hud1");
+            this.hud1.eahrsstatus = ((uint)(0u));
             this.hud1.ekfstatus = 0F;
-            this.hud1.eahrsstatus = 0;
             this.hud1.failsafe = false;
             this.hud1.gpsfix = 0F;
             this.hud1.gpsfix2 = 0F;
@@ -2621,7 +2622,8 @@ namespace MissionPlanner.GCSViews
             this.takeOffToolStripMenuItem,
             this.onOffCameraOverlapToolStripMenuItem,
             this.jumpToTagToolStripMenuItem,
-            this.gimbalVideoToolStripMenuItem});
+            this.gimbalVideoToolStripMenuItem,
+            this.imHereToolStripMenuItem});
             this.contextMenuStripMap.Name = "contextMenuStrip1";
             resources.ApplyResources(this.contextMenuStripMap, "contextMenuStripMap");
             // 
@@ -2772,6 +2774,12 @@ namespace MissionPlanner.GCSViews
             // 
             resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
+            // 
+            // imHereToolStripMenuItem
+            //
+            this.imHereToolStripMenuItem.Name = "imHereToolStripMenuItem";
+            resources.ApplyResources(this.imHereToolStripMenuItem, "imHereToolStripMenuItem");
+            this.imHereToolStripMenuItem.Click += new System.EventHandler(this.imHereToolStripMenuItem_Click);
             // 
             // but_disablejoystick
             // 
@@ -3315,5 +3323,6 @@ namespace MissionPlanner.GCSViews
         private Controls.MyButton BUT_externalAHRS_start;
         private Controls.MyButton BUT_externalAHRS_stop;
         private Controls.MyButton BUT_externalAHRS_aiding_data;
+        private ToolStripMenuItem imHereToolStripMenuItem;
     }
 }
