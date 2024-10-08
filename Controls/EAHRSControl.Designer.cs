262ec67f9675b7865c314322aa8fc7fc63115d0f
Valentin Bugrov <valentin.bugrov@inertiallabs.com>
2024-10-08 15:52:18 +0400

    EAHRSControl: Add display of GPS/GNSS diff and accuracy

--- Controls/EAHRSControl.Designer.cs
+++ Controls/EAHRSControl.Designer.cs
@@ -44,5 +44,6 @@
         private QuickView quickView4;
         private QuickView quickView5;
         private QuickView quickView6;
+        private System.Windows.Forms.Timer posUpdateTimer;
     }
 }
