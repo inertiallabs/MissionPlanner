646be3fb3747aede5c3868077f98c0974d15df70
Valentin Bugrov <valentin.bugrov@inertiallabs.com>
2024-10-28 18:08:11 +0400

    Common: Add GPS raw data marker for other vehicles

--- ExtLibs/Maps/Resources.Designer.cs
+++ ExtLibs/Maps/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace MissionPlanner.Maps {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Resources {
@@ -1645,6 +1645,16 @@ namespace MissionPlanner.Maps {
         public static System.Drawing.Bitmap rover_11 {
             get {
                 object obj = ResourceManager.GetObject("rover_11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap rover_2 {
+            get {
+                object obj = ResourceManager.GetObject("rover_2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
