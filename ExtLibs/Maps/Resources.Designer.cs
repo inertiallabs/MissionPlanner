ab89599fb08ea0b56a7a3152e09b84a55f5204ab
Valentin Bugrov <vladbvnsk@gmail.com>
2025-12-08 17:29:12 -0500

    Maps: Extend vechicle markers by other colors

--- ExtLibs/Maps/Resources.Designer.cs
+++ ExtLibs/Maps/Resources.Designer.cs
@@ -309,7 +309,17 @@ namespace MissionPlanner.Maps {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap boat_2 {
+            get {
+                object obj = ResourceManager.GetObject("boat_2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
@@ -1645,6 +1655,16 @@ namespace MissionPlanner.Maps {
         public static System.Drawing.Bitmap rover_11 {
             get {
                 object obj = ResourceManager.GetObject("rover_11", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap rover_2 {
+            get {
+                object obj = ResourceManager.GetObject("rover_2", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
